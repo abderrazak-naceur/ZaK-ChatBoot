528e4da59d361a944a32c67fe774968d2587118f
Abderrazak Naceur <therzeos@gmail.com>
2018-09-01 00:53:36 +0200

    b

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,40 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'example/main.jsx'),
+  entry: path.resolve(__dirname, 'app/js/main.js'),
   output: {
-    path: path.resolve(__dirname, 'example'),
+    path: path.resolve(__dirname, 'docs/'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'example'),
-    host: '0.0.0.0',
-    disableHostCheck: true,
+    contentBase: path.join(__dirname, 'app'),
+    compress: true,
+    port: 8080,
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  plugins: [],
-  devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        context: path.resolve(__dirname, 'app/'),
+        from: 'index.html',
+        to: path.resolve(__dirname, 'docs'),
+      },
+    ]),
+  ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$|.jsx$/,
         exclude: /(node_modules|bower_components)/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
